f9f97eee5d966da511d7876e80159e2949434eb6
txt <txt1919810@gmail.com>
2025-12-29 02:42:17 +0800

    新版

--- src/serial_dispose/serial_dispose/serial_dispose.py
+++ src/serial_dispose/serial_dispose/serial_dispose.py
@@ -9,60 +9,62 @@ import sys
 import numpy as np
 
 from get_dispose_serial.myserial import AsyncSerial_t 
-from get_dispose_serial.in_dispose import dispose_serial_init
 class SerialDisposeNode(Node):
     def __init__(self):
         super().__init__('serial_dispose_node')
-        self.serial = dispose_serial_init(
+        self.serial = AsyncSerial_t(
             port="/dev/tnt1",
             baudrate=115200,
-            callback=self.on_odom
         )
+        self.serial.startListening(lambda data: self.serial_callback(data))
         self.publisher_ = self.create_publisher(Vector3Stamped, 'lidar_position', 10)
         self.publisher_odom = self.create_publisher(Vector3Stamped, 'odom_data', 10)
-        #self.sub_cmd_vel = self.create_subscription(Twist, self.get_parameter('cmd_vel'), 10)
-        self.topic_map = {
-            "/cmd_vel": {
-                "msg_type": Twist,
-                "func": 0xA0,
-                "encoder": self.encode_cmd_vel
-            },
-            "/robot_state": {
-                "msg_type": String,
-                "func": 0xA0,
-                "encoder": self.encode_robot_state
-            }
+        self.subscription_cmd_vel = self.create_subscription(Twist,'cmd_vel',self.cmd_vel_callback,10)
+
+        self.out_first_frame = b'\xFA'
+        self.out_frame_list = {
+            "joy_cmd": {
+            "id": b'\xAA',
+            "fmt": "<fff"
         }
-        self.subs = []
-        for topic, cfg in self.topic_map.items():
-            sub = self.create_subscription(
-                cfg["msg_type"],
-                topic,
-                lambda msg, t=topic: self.topic_callback(t, msg),
-                10
-            )
-            self.subs.append(sub)
-    def build_frame(self, func: int, payload: bytes) -> bytes:
-        return b'\xFA' + bytes([func]) + payload    
-    def topic_callback(self, topic, msg):
-        cfg = self.topic_map[topic]
-        func = cfg["func"]
-        payload = cfg["encoder"](msg)
-        frame = self.build_frame(func, payload)
-        self.serial.write(frame)
-   
-    def encode_cmd_vel(self, msg: Twist) -> bytes:
-        return struct.pack("<fff",msg.linear.x,msg.linear.y,msg.angular.z)
-    def encode_robot_state(self, msg: String) -> bytes:
-        return struct.pack("<fff",0.0,0.0,0.0)
-    
-    def on_odom(self, x: float, y: float, yaw: float):
+}
+        self.in_first_frame = b'\xFF'
+        self.in_frame_list = {
+            b'\xA0': {
+                "name": "odom_frame",
+                "dispose": self.dispose_odom,
+                "fmt": "<fff"
+            },
+        }   
+    def cmd_vel_callback(self, msg):
+        linear_x = msg.linear.x
+        linear_y = msg.linear.y
+        angular_z = msg.angular.z
+        data = struct.pack(self.out_frame_list["joy_cmd"]["fmt"], linear_x,linear_y, angular_z)
+        send_joy = self.out_first_frame + self.out_frame_list["joy_cmd"]["id"] + data
+        self.serial.write(send_joy)
+    def serial_callback(self, data):
+        if len(data) < 2:
+            return
+        if data[0:1] != self.in_first_frame:
+            return
+        frame_id = data[1:2]
+        frame_data = self.in_frame_list.get(frame_id)
+        if frame_data is None:
+            return
+        else :
+            if len(data) < 2 + struct.calcsize(frame_data["fmt"]):
+                return
+            get_data = frame_data["dispose"](data[2:2 + struct.calcsize(frame_data["fmt"])],frame_data["fmt"])
+            self.publisher_.publish(get_data)
+    def dispose_odom(self, data, fmt):
+        x, y, z = struct.unpack(fmt, data)
         msg = Vector3Stamped()
-        msg.header.stamp = self.get_clock().now().to_msg()
         msg.vector.x = x
         msg.vector.y = y
-        msg.vector.z = yaw
-        self.publisher_odom.publish(msg)
+        msg.vector.z = z
+        return msg
+        
 def main(args=None):
     rclpy.init(args=args)
     serial_dispose_node = SerialDisposeNode()
